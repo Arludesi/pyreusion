bcd4664ab120de6f80b79bb75d9f554dae637e14
JoeYoung <1022104172@qq.com>
2022-12-01 15:41:37 +0800

    1.0.3

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Pyreusion",
-    version="1.0.2.3",
+    name="pyreusion",
+    version="1.0.3",
     author="JoeYoung",
     author_email="1022104172@qq.com",
     description="Common method integration in the process of Python programming.",
     url="https://github.com/Arludesi/pyreusion.git", 
     python_requires='>=3.6',
+    classifiers=['License :: OSI Approved :: GNU General Public License version 3',],
     packages=find_packages(),
 
     tests_require=[
         'pytest>=3.3.1',
     ],
 
-    package_data={
-        '':['pyreusion.py'],
-               },
+    # package_data={
+    #     '': ['*.*'],
+    #            },
 
     # exclude_package_data={
-    #     'bandwidth_reporter':['*.txt']
+    #     '*':['*.bat']
     #            }
 )
